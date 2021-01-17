4d48dfd85cea1ad986d6dad18babbf7fad70076a
Arif ORAK <ariforak91@gmail.com>
2021-01-17 18:41:40 +0300

    17.01.2021 Ödev Eklendi (Ternany)

---
+++ TernanyOdev/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TernanyOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Şart blokları yerine tek satırda durumları kontrol etmemize yaramaktadır.
+            // Kural olarak değişkene atamasını istediğimiz değer eğer değişkenin veri tipi string ise Operatörlerin döndüreceği tip de string olmalıdır..!
+            bool medeniHal = true;
+            string mesaj = medeniHal == true ? "Evlilere Kampanya" : "Bekarlara Kampanya";
+            Console.WriteLine(mesaj);
+        }
+    }
+}
