ca78251095896cc91ef320ebc7fb2188a575e5b6
Arif ORAK <ariforak91@gmail.com>
2021-01-17 14:55:17 +0300

    Son güncelleme !!! 17.01.2021

---
+++ Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Daha önce oluşturduğumuz ürün classından yeni bir ürün türettik.
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Portakal";
+            urun2.Fiyati = 25;
+            urun2.Aciklama = "Finike Portakalı";
+
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine("Ürün Adı : " + urun.Adi + " : " + "Ürün Fiyatı :" + urun.Fiyati);
+            }
+
+            Console.WriteLine("---------------Metotlar------------------");
+
+            //İnstance  -- Class ÖRNEĞİ
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Kiraz", "Kırmızı Kiraz", 15, 3);
+                  
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
