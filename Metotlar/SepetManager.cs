a79964dfec89d058401d780254abdf18a9afc747
Arif ORAK <ariforak91@gmail.com>
2021-01-18 20:49:31 +0300

    Prg.cs

--- Metotlar/SepetManager.cs
+++ Metotlar/SepetManager.cs
@@ -7,12 +7,13 @@ namespace Metotlar
     class SepetManager
     {
         //naming convention  -- Fonksiyon = Metot
-        public void Ekle(Urun urun) 
+        public void Ekle(Urun urun)
         {
-            Console.WriteLine("Tebrikler..Sepete Eklendi : " + urun.Adi);        
+            Console.WriteLine("Tebrikler..Sepete Eklendi : " + urun.Adi);
         }
-        public void Ekle2(string urunAdi, string urunAciklamasi, int urunFiyati, int stokAdedi) 
+        public void Ekle2(string urunAdi, string urunAciklamasi, int urunFiyati, int stokAdedi)
         {
             Console.WriteLine("Sepete Eklendi!" + urunAdi);
-        }    
+        }
+    }
 }
---
+++ SwitchCaseArastirmaOdev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchCaseArastirmaOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
